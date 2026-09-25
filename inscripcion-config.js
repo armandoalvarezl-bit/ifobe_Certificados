53e1350739bc198aa475dbe11423959b096eae7a
armandoalvarezl-bit <armando.alvarezl@cun.edu.co>
2026-09-24 19:57:26 -0500

    Add files via upload

---
+++ inscripcion-config.js
@@ -0,0 +1,5 @@
+// URL /exec de la plataforma tras publicar backend/Code.gs integrado.
+// Ejecutar configurarBaseDatos y publicar la implementación antes de usar el portal.
+window.IFOBE_INSCRIPCION = Object.freeze({
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzK06Pj2dkPWoSB0b5hMfC0mXbC14HUqyBgEbYPrnSNHaJPinbAgJVKuK3Dv0DVcHLc5g/exec'
+});
